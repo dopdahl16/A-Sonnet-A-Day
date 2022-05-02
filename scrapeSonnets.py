6672f51fc06347097bad7186c73b1550c268f290
Daniel Opdahl <dopdahl16@gmail.com>
2022-05-02 10:27:35 -0600

    Rework, got sonnet text cleaned

--- scrapeSonnets.py
+++ scrapeSonnets.py
@@ -1,9 +1,14 @@
 import requests
 import random
+import re
 
-URL = 'http://www.shakespeares-sonnets.com/all.php'
+#URL = 'http://www.shakespeares-sonnets.com/all.php'
+
+sonnet_no = random.randint(1, 154)
+URL = "http://www.shakespeares-sonnets.com/sonnet/" + str(sonnet_no)
 page = requests.get(URL)
-text = page.text
-c = text.split("<a href='/sonnet/")
-print(page.text)
-a = random.randint(1, 154)
+sonnet_text = page.text.split("<h1 class='title'")[1].split("<div class='separator")[0].split(">", 1)[1]
+
+sonnet_text = re.sub('<[^<]+?>', '', sonnet_text).replace("&nbsp;", "  ")
+print(sonnet_text)
+print(URL)
